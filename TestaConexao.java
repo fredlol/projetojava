e4bc63f6a7b2d6f5465ae5e6649d99273058ae63
fredlol <wirifrits@hotmail.com>
2017-06-24 19:54:16 -0300

    Update TestaConexao.java

--- TestaConexao.java
+++ TestaConexao.java
@@ -2,6 +2,7 @@ package factory;
 import java.sql.Connection;
 import java.sql.SQLException;
 public class TestaConexao {
+   //testar a conexão com o banco
    /* public static void main(String[] args) throws SQLException {
         Connection connection = new ConnectionFactory().getConnection();
         System.out.println("Conexão aberta!");
